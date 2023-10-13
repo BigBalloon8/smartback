05106dbf85d5d9c92a57583ba006ebcdca8313dc
Christopher Rae <raecd123@gmail.com>
2023-10-13 12:35:30 +0100

    softmax jacobian implemented

--- experiment2v2(model_parallelism)/layers.py
+++ experiment2v2(model_parallelism)/layers.py
@@ -1,4 +1,7 @@
 import torch
+from torch import vmap as vmap
+import numpy as np
+
 from abc import ABC, abstractmethod
 
 class Layer(ABC):
@@ -94,6 +97,7 @@ class MultiHeadAttention(Layer):
                        "V": torch.zeros_like(V)}
         
         out = self.forward(Q, K, V, mask)
+        self.sQK_T = torch.zeros(1) # TODO update this
         self.out = torch.zeros_like(out)
         self.dL_dout = torch.zeros_like(out)
         return out
@@ -107,30 +111,66 @@ class MultiHeadAttention(Layer):
         lK = self.linears["K"](K)
         lV = self.linears["V"](V)
         
-        lQ = torch.cat(lQ.unsqueeze(-2).chunk(num_heads, dim=-1), dim=-2)
-        lK = torch.cat(lK.unsqueeze(-2).chunk(num_heads, dim=-1), dim=-2)
-        lV = torch.cat(lV.unsqueeze(-2).chunk(num_heads, dim=-1), dim=-2)
+        lQ = torch.cat(lQ.unsqueeze(-2).chunk(self.num_heads, dim=-1), dim=-2)
+        lK = torch.cat(lK.unsqueeze(-2).chunk(self.num_heads, dim=-1), dim=-2)
+        lV = torch.cat(lV.unsqueeze(-2).chunk(self.num_heads, dim=-1), dim=-2)
         
-        QK_T = torch.vmap(lambda q, k: torch.mm(q, torch.transpose(k, -1, -2)), in_dims=-2, out_dims=-2)(Q, K)
-        QK_T = torch.vmap(lambda x: x + mask, in_dims=-2, out_dims=-2)(QK_T)
-        sQK_T = lambda x: torch.softmax(x, dim=-1)(QK_T) #TODO add inplace update
+        QK_T = vmap(lambda q, k: torch.mm(q, torch.transpose(k, -1, -2)), in_dims=-2, out_dims=-2)(Q, K)
+        QK_T = vmap(lambda x: x + mask, in_dims=-2, out_dims=-2)(QK_T)
+        torch.softmax(QK_T, dim=-1, out=self.sQK_T) #TODO add inplace update
         
-        out = torch.vmap(lambda qk_t, v: torch.mm(qk_t, v), in_dims=-2, out_dims=-2)(sQK_T, V)
+        out = vmap(lambda qk_t, v: torch.mm(qk_t, v), in_dims=-2, out_dims=-2)(self.sQK_T, V)
         self.out[:] = out.reshape(out.shape[:2], -1)
         return self.out
     
+    @staticmethod
+    def _softmax_jacobian(softmax_out: torch.Tensor): #softmax_out.shape -> N | 1xN
+        softmax_out = torch.squeeze(softmax_out)
+        jac_base = torch.arange(softmax_out.shape[-1]**2, dtype=torch.int)
+        
+        def _jacobian(index):
+            row = index // softmax_out.shape[-1]
+            col = index - row*softmax_out.shape[-1]
+            #print(row, col)
+            if row != col:
+                return -softmax_out[row]*softmax_out[col]
+            else:
+                return softmax_out[row]*(1-softmax_out[col])
+        
+        jac_base = np.vectorize(_jacobian)(jac_base) #TODO find a better way of doing this
+        return jac_base.reshape((softmax_out.shape[-1],)*2)
+
+        
     def backward_p1(self, dL_dout):
         self.dL_dout[:] = dL_dout
         
-        dL_dout = torch.cat(dL_dout.unsqueeze(-2).chunk(num_heads, dim=-1), dim=-2)
+        dL_dout = torch.cat(dL_dout.unsqueeze(-2).chunk(self.num_heads, dim=-1), dim=-2)
         
-        lV = torch.cat(self.linears["V"].out.unsqueeze(-2).chunk(num_heads, dim=-1), dim=-2)
-        dL_dsQKT = torch.vmap(lambda dl_dout, v: torch.mm(dl_dout, torch.transpose(v, -1,-2)), in_dims=-2, out_dims=-2)(dL_dout, lV)
-        J_sQKT = 
+        lV = torch.cat(self.linears["V"].out.unsqueeze(-2).chunk(self.num_heads, dim=-1), dim=-2)
+        dL_dsQKT = vmap(lambda dl_dout, v: torch.mm(dl_dout, torch.transpose(v, -1,-2)), in_dims=-2, out_dims=-2)(dL_dout, lV)
+        
+        # vmap across 3 dims BxCxH 
+        J_sQKT = vmap(vmap(vmap(self._softmax_jacobian)))(self.sQK_T)  # sQK_T.shape -> BxCxHxC 
+        dL_dQKT = vmap(vmap(vmap(torch.mm)))(dL_dsQKT, J_sQKT)
+        
+        dL_dlQ = ...
+        dL_dlK = ...
+        
+        dL_dQ = self.linears["Q"].backward_p1(dL_dlQ)
+        dL_dK = self.linears["K"].backward_p1(dL_dlK)
+        dL_dV = self.linears["V"].backward_p1(dL_dout)
+        
+        return dL_dQ, dL_dK, dL_dV
         
         
+    def backward_p2(self):
+        return super().backward_p2()    
+    
         
-        
+if __name__ == "__main__":
+    layer = MultiHeadAttention(10, 8)
+    b = torch.softmax(torch.randn(10), dim=-1)
+    print(layer._softmax_jacobian(b))
         
         
     
